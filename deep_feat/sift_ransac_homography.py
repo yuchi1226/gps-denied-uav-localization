63c6223345745205cbbd8e76540f44c357e4ed94
YUCHI <kirklan0423.gmail.com>
2025-08-16 10:33:41 +0800

    update sift_ransac_homography.py

--- deep_feat/sift_ransac_homography.py
+++ deep_feat/sift_ransac_homography.py
@@ -101,8 +101,8 @@ def get_param(img_batch, template_batch, image_sz):
 		# store all the good matches as per Lowe's ratio test
 		good = []
 		for m,n in matches:
-		    if m.distance < 0.7*n.distance:
-		        good.append(m)
+			if m.distance < 0.7*n.distance:
+				good.append(m)
 
 		src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
 		dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
